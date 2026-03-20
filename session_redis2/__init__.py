f925f48aabb644e1514f390da13357a1492f8914
gridpaneburti-ship-it <gridpaneburti@gmail.com>
2026-03-20 14:05:53 +0100

    Update __init__.py

--- session_redis2/__init__.py
+++ session_redis2/__init__.py
@@ -1,2 +1,6 @@
 from . import http
 from . import session
+import os
+os.environ['ODOO_SESSION_REDIS'] = '1'
+os.environ['ODOO_SESSION_REDIS_HOST'] = 'odoo-redis'
+os.environ['ODOO_SESSION_REDIS_PORT'] = '6379'
