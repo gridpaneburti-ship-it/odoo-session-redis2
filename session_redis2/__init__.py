49dd6702eac91664cb9e82e7f6cadff0c5838873
gridpaneburti-ship-it <gridpaneburti@gmail.com>
2026-03-20 14:30:31 +0100

    Update __init__.py

--- session_redis2/__init__.py
+++ session_redis2/__init__.py
@@ -1,6 +1,10 @@
+import os
+
+# 1. Configurar el entorno ANTES de cargar el código del módulo
+os.environ.setdefault('ODOO_SESSION_REDIS', '1')
+os.environ.setdefault('ODOO_SESSION_REDIS_HOST', 'odoo-redis')
+os.environ.setdefault('ODOO_SESSION_REDIS_PORT', '6379')
+
+# 2. Ahora sí, cargar el resto del módulo que leerá esas variables
 from . import http
 from . import session
-import os
-os.environ['ODOO_SESSION_REDIS'] = '1'
-os.environ['ODOO_SESSION_REDIS_HOST'] = 'odoo-redis'
-os.environ['ODOO_SESSION_REDIS_PORT'] = '6379'
